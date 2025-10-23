25f5136e01ad4300678cd1fc735dbbe36112e143
tomass <dominanthat@gmail.com>
2025-10-23 21:05:17 +0900

    Feat: 간단한 krds 컴포넌트 출력 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import Typography from './components/Typography'
+import { Button } from '@krds-ui/core'
 
 export default function App() {
   return (
-    <Typography variant="display" size="large">
-      대한민국 전자정부 사이트 예제입니다.
-    </Typography>
+    <>
+      <Typography variant="display" size="large">
+        대한민국 전자정부 사이트 예제입니다.
+      </Typography>
+      <Button
+        onClick={function ra(){}}
+        variant="primary"
+      >
+        버튼: Primary
+      </Button>
+    </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+// main.tsx
+import '@krds-ui/core/dist/style.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
