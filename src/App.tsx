0f7b9310351e605139a244fadb77d4f55db13f1d
tomass <dominanthat@gmail.com>
2025-11-02 04:22:57 +0900

    Fix: 메인 컴포넌트 간단 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import Typography from './components/Typography'
 import { Footer } from './components/Footer'
-import { Button } from '@krds-ui/core'
 import { MainBanner } from './components/MainBanner'
 
 export default function App() {
   return (
     <>
-      <Typography variant="display" size="large">
-        대한민국 전자정부 사이트 예제입니다.
-      </Typography>
-      <Button onClick={function ra() {}} variant="primary">
-        버튼: Primary
-      </Button>
+      <Typography variant="display" size="large"><></></Typography>
+      
       <main>
         <MainBanner />
       </main>
