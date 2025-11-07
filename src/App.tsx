8b4217fcff6726488544541ae23d698495ab8cb4
Jisub <qkrwltjq2426@naver.com>
2025-11-07 10:43:34 +0900

    design: 헤더 krds 컴포넌트로 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Typography from './components/Typography'
 import { Footer } from './components/Footer'
 import { MainBanner } from './components/MainBanner'
+import { Header } from './components/Header'
 
 export default function App() {
   return (
     <>
-      <Typography variant="display" size="large"><></></Typography>
-      
+      <Typography variant="display" size="large">
+        <></>
+      </Typography>
+      <header>
+        <Header />
+      </header>
+
       <main>
         <MainBanner />
       </main>
